e57044dfac7ffff8c7f78806273baa2a310bba43
Leonid Feskov <leonik89@gmail.com>
2017-02-01 18:53:56 +0300

    Load image

--- js/main.js
+++ js/main.js
@@ -2,17 +2,41 @@
 var string = '@#%=+*:-. ';
 var ASCIIColors = string.split('');
 var limit = 1000000;
-var pictures = ['necr.png', 'venom.png', 'axe.png', 'sven.png', 'pudge.png', 'never.png', 'lich.png',  'zombi.png', 'darkseer.png', 'ursa.png', 'lina.png', 'invoker.png', 'balanar.png', 'kunka.png', 'spektra.png', 'puck.png'];
-var activePic = 0;
+var maxWidth = 120;
+var maxHeight = 120;
 
-var loadTexture = function (name, then) {
+
+var inputFile = document.querySelector('.js-file');
+var preview = document.querySelector('.js-preview');
+var asciiImage = document.querySelector('.js-ascii-image');
+
+function drawAsciiImage(data){
+
+	console.log('draw:', data);
+	var colors = data.data;
+	var str = '';
+
+	for (var i = 0; i < colors.length; i++) {
+		var color = colors[i];
+		if ( i > 0 && (i % data.width) == 0 ) {
+			str += '<br>';
+		}
+		var symbol = ASCIIColors[color > 0 ? Math.ceil(color/255*ASCIIColors.length)-1 : 0];
+		str += symbol+''+symbol;
+
+	}
+	asciiImage.innerHTML = str;
+}
+
+var getPixelsColorCode = function(name) {
 	var texture = new Image();
 	var result = {};
 
-	// on load
+	texture.src = name;
+
 	texture.addEventListener('load', function () {
 		if (this.width * this.height > limit) {
-			alert('Изображение слишком большое. Максимальная плотность изображения '+limit+'px.');
+			alert('Изображение слишком большое. Максимальная плотность изображения ' + limit + 'px.');
 			return false;
 		}
 
@@ -30,53 +54,53 @@ var loadTexture = function (name, then) {
 		var imageData = context.getImageData(0, 0, this.width, this.height);
 		var data = imageData.data;
 
+		console.log('data:', data);
+
+		// преобразуем изображение в градации серого
 		for(var i = 0; i < data.length; i += 4) {
 			var brightness = 0.34 * data[i] + 0.5 * data[i + 1] + 0.16 * data[i + 2];
 			data[i] = brightness;
 			data[i + 1] = brightness;
 			data[i + 2] = brightness;
 		}
-
-		// overwrite original image
 		context.putImageData(imageData, 0, 0);
 
-		result.data = context.getImageData(0, 0, this.width, this.height).data;
+		// каждый пиксел предствлен 4-мя значениями, например [34, 34, 34, 255];
+		// нас интересует только первое число
+		var grayData = context.getImageData(0, 0, this.width, this.height).data;
+		console.log('grayData:', grayData);
 
-		then();
+		var allPixels = grayData.filter(function(pixel, i){
+			return i % 4 === 0;
+		});
+
+		var ratioWidth = Math.round(this.width / maxWidth);
+		console.log(ratioWidth);
+
+		// сжимаем изображение до размеров maxWidth * maxHeight
+		result.data = allPixels.filter(function(pixel, i){
+			return true;
+		});
+
+		drawAsciiImage(result);
 	}, false);
-
-	texture.src = name;
-	return result;
 };
 
-function drawPicture(data){
-	var colors = data.data;
-	var pic = document.querySelector('.pic');
-	var str = '';
+function previewImage() {
+	var file = inputFile.files[0];
+	var reader = new FileReader();
 
-	for (var i = 0; i < colors.length; i+=4) {
-		var color = colors[i];
-		if ( i > 0 && (i % (data.width*4)) == 0 ) {
-			str += '<br>';
-		}
-		var symbol = ASCIIColors[color > 0 ? Math.ceil(color/255*ASCIIColors.length)-1 : 0];
-		str += symbol+''+symbol;
+	reader.onloadend = function() {
+		preview.src = reader.result;
 
+		getPixelsColorCode(preview.src);
+	};
+
+	if (file) {
+		reader.readAsDataURL(file);
+	} else {
+		preview.src = '';
 	}
-	pic.innerHTML = str;
 }
 
-function loadPicture(src){
-	var texture = loadTexture(src, function () {
-		if (texture) {
-			drawPicture(texture);
-		}
-	});
-}
-
-document.addEventListener('click', function(){
-	loadPicture('i/' + pictures[activePic++]);
-	if (activePic >= pictures.length) {
-		activePic = 0;
-	}
-});
+inputFile.addEventListener('change', previewImage);
