7a07fb07786abb41717d5891f2fa541e3470a2fa
Leonid Feskov <leonik89@gmail.com>
2017-02-03 12:24:31 +0300

    Optimize render

--- js/main.js
+++ js/main.js
@@ -1,30 +1,33 @@
 //var string = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`. ';
 var string = '@#%=+*:-. ';
 var ASCIIColors = string.split('');
-var limit = 1000000;
-var maxWidth = 120;
-var maxHeight = 120;
-
+var limit = 4000000;
+var maxWidth = 150;
+var maxHeight = 150;
 
 var inputFile = document.querySelector('.js-file');
 var preview = document.querySelector('.js-preview');
 var asciiImage = document.querySelector('.js-ascii-image');
 
-function drawAsciiImage(data){
+function drawAsciiImage(colors, width, height){
+	// если изображение больше maxWidth * maxHeight px, то не рисуем каждый пиксель, а рисуем с шагом ratioWidth
+	var ratioWidth = Math.ceil(width / maxWidth);
+	var ratioHeight = Math.ceil(width / maxHeight);
 
-	console.log('draw:', data);
-	var colors = data.data;
 	var str = '';
 
-	for (var i = 0; i < colors.length; i++) {
-		var color = colors[i];
-		if ( i > 0 && (i % data.width) == 0 ) {
-			str += '<br>';
-		}
-		var symbol = ASCIIColors[color > 0 ? Math.ceil(color/255*ASCIIColors.length)-1 : 0];
-		str += symbol+''+symbol;
+	for(var i = 0; i < height; i += ratioHeight) {
+		var row = colors[i];
 
+		for(var j = 0; j < width; j += ratioWidth) {
+			var color = row[j];
+			var symbol = ASCIIColors[color > 0 ? Math.ceil(color / 255 * ASCIIColors.length) - 1 : 0];
+			str += symbol + '' + symbol;
+		}
+
+		str += '<br>';
 	}
+
 	asciiImage.innerHTML = str;
 }
 
@@ -42,48 +45,32 @@ var getPixelsColorCode = function(name) {
 
 		var canvas = document.createElement('canvas');
 		var context = canvas.getContext('2d');
+		var width = this.width;
+		var height = this.height;
 		
-		canvas.width = this.width;
-		canvas.height = this.height;
-
-		result.width = this.width;
-		result.height = this.height;
+		canvas.width = width;
+		canvas.height = height;
 
 		context.drawImage(this, 0, 0);
 
 		var imageData = context.getImageData(0, 0, this.width, this.height);
 		var data = imageData.data;
 
-		console.log('data:', data);
-
-		// преобразуем изображение в градации серого
+		// каждый пиксел предствлен 4-мя значениями, например [34, 136, 204, 255];
+		// используя первые три значения, получаем нужный оттенок серого цвета.
+		var pixelMatrix = [];
+		var row = 0;
+		pixelMatrix[0] = [];
 		for(var i = 0; i < data.length; i += 4) {
-			var brightness = 0.34 * data[i] + 0.5 * data[i + 1] + 0.16 * data[i + 2];
-			data[i] = brightness;
-			data[i + 1] = brightness;
-			data[i + 2] = brightness;
+			if ( (i / (row + 1)) >= (width * 4) ) {
+				pixelMatrix[++row] = [];
+			}
+			var brightness = Math.round(0.34 * data[i] + 0.5 * data[i + 1] + 0.16 * data[i + 2]);
+			pixelMatrix[row].push(brightness);
 		}
-		context.putImageData(imageData, 0, 0);
 
-		// каждый пиксел предствлен 4-мя значениями, например [34, 34, 34, 255];
-		// нас интересует только первое число
-		var grayData = context.getImageData(0, 0, this.width, this.height).data;
-		console.log('grayData:', grayData);
-
-		var allPixels = grayData.filter(function(pixel, i){
-			return i % 4 === 0;
-		});
-
-		var ratioWidth = Math.round(this.width / maxWidth);
-		console.log(ratioWidth);
-
-		// сжимаем изображение до размеров maxWidth * maxHeight
-		result.data = allPixels.filter(function(pixel, i){
-			return true;
-		});
-
-		drawAsciiImage(result);
-	}, false);
+		drawAsciiImage(pixelMatrix, width, height);
+	});
 };
 
 function previewImage() {
